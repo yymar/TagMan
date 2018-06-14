5f4e81a0870fde6d085bc0c5139e968c15e9ccd9
yymar <yoran.de.haan@gmaill.com>
2018-06-14 10:45:44 +0200

    :fire: remove syso's

--- TagMan_Blok4_yorandehaan_2124346/src/view/TimeView.java
+++ TagMan_Blok4_yorandehaan_2124346/src/view/TimeView.java
@@ -104,6 +104,5 @@ public class TimeView extends JPanel implements Observer {
 	@Override
 	public void update(Observable o, Object arg) {
 		secondsInt.setText(arg + "");
-		System.out.println(arg);
 	}
 }
